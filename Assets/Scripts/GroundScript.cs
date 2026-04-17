231de42a43a188b10eff2020e4d42e21ebf54cda
vegboorgir <agrawalkartik800@gmail.com>
2026-04-17 21:59:00 +0530

    fix ground regen bug

--- Assets/Scripts/GroundScript.cs
+++ Assets/Scripts/GroundScript.cs
@@ -1,29 +1,32 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class GroundScript : MonoBehaviour
 {
     [SerializeField] Transform player;
-    [SerializeField] float groundLength = 6f;
+    [SerializeField] float groundLength = 40f;
+    [SerializeField] float regenThreshold = 20f; // Distance from tile end to trigger regen
 
     private Transform[] grounds;
     private int lastGroundIndex = 0;
+    
     void Start()
     {
         grounds = new Transform[transform.childCount];
-        for (int i = 0; i<transform.childCount; i++)
+        for (int i = 0; i < transform.childCount; i++)
         {
             grounds[i] = transform.GetChild(i);
         }
-
     }
 
     void Update()
     {
-        if(player.position.z - grounds[lastGroundIndex].position.z > groundLength)
+        float distanceToEnd = grounds[lastGroundIndex].position.z + groundLength - player.position.z;
+        
+        // Regenerate when player is within regenThreshold of the tile's end
+        if (distanceToEnd <= regenThreshold)
         {
             int nextIndex = (lastGroundIndex + 1) % grounds.Length;
-            grounds[lastGroundIndex].position = grounds[nextIndex].position + new Vector3(0, 0 ,groundLength);
+            grounds[lastGroundIndex].position = grounds[nextIndex].position + new Vector3(0, 0, groundLength);
             lastGroundIndex = nextIndex;
         }
     }
