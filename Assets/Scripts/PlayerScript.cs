c0b7d58576ed2a7287a1efcf01ef38105bffc860
vegboorgir <agrawalkartik800@gmail.com>
2026-04-17 22:29:48 +0530

    add lane switching

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,13 +1,21 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
 {
     [SerializeField] float speed = 5f;
     [SerializeField] float jumpForce = 5f;
-    private Rigidbody rb;
+    [SerializeField] float laneDistance = 3f;
+    [SerializeField] float laneSwitchSpeed = 10f;
+    [SerializeField] float switchCooldown = 0.2f;
 
+    private Rigidbody rb;
     private Vector2 _moveDirection;
+    private int currentLane = 1;
+    private float targetXPosition;
+    private float coolDownTimer = 0f;
+
 
     public InputActionReference moveRL;
     public InputActionReference jump;
@@ -17,6 +25,7 @@ public class PlayerScript : MonoBehaviour
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        targetXPosition = transform.position.x;
     }
 
     void FixedUpdate()
@@ -26,7 +35,27 @@ public class PlayerScript : MonoBehaviour
 
     void Update()
     {
-         _moveDirection = moveRL.action.ReadValue<Vector2>();
+        if(coolDownTimer > 0)
+            coolDownTimer -= Time.deltaTime;
+
+        _moveDirection = moveRL.action.ReadValue<Vector2>();
+
+        if(coolDownTimer <= 0 && Mathf.Abs(_moveDirection.x) > 0.5f)
+        {
+            int laneChange = _moveDirection.x > 0 ? 1 : -1;
+            int newLane = currentLane + laneChange;
+
+            if(newLane >= 0 && newLane <= 2)
+            {
+                currentLane = newLane;
+                targetXPosition = (currentLane - 1) * laneDistance;
+                coolDownTimer = switchCooldown;
+            }
+        }
+
+        Vector3 newPosition = transform.position;
+        newPosition.x = Mathf.Lerp(transform.position.x, targetXPosition, laneSwitchSpeed * Time.deltaTime);
+        transform.position = newPosition;
     }
 
     private void OnEnable()
